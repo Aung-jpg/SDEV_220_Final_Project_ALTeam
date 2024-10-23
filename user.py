79cdb2ddf679d5142634c81ee72e60ea98a168b9
Logan Ghast <loganghast06@gmail.com>
2024-10-23 13:57:09 -0400

    changing argument to a dict and adding comment

--- user.py
+++ user.py
@@ -1,9 +1,10 @@
 class User():
-    """User calss will interact with a Computer_reservation class to reverse a computer"""
-    def __init__(self, library_card_number, pin) -> None:
-        self.library_card_number = library_card_number
-        # eventually make pin more secure (maybe by hashing it)
-        self.pin = pin
+    """User class will interact with a ComputerReservation class to reverse a computer"""
+    def __init__(self, data:dict) -> None:
+        """data is a dict with data["library_card_number"] and data["pin"]"""
+        self.library_card_number = data["library_card_number"]
+        # eventually make pin more secure (maybe by hashing it) or getting it as an input from UI
+        self.pin = data["pin"]
         # get other data (like name) by querying the user database
     
     def __str__(self) -> str:
