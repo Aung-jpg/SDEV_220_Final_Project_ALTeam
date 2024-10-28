6ecf27c7b2f2baee6cb7055113a1be5782cd17f3
LoganGhast <142341592+Obeth297@users.noreply.github.com>
2024-10-28 13:58:44 -0400

    start of the reservation class

--- reservation.py
+++ reservation.py
@@ -1,9 +1,15 @@
-class ComputerReservation():
-    def __init__(self, library_card_num:str) -> None:
-        if library_card_num not in []: # not in database for library card nums
-            raise "ERROR: number is not in system"
-        self.card_number = library_card_num
-        # get other data by querying database
+from user import User
+class ComputerReservation:
+    def __init__(self, library_card_number):
+        self.reservations = {}
+        self.user = library_card_number
 
-    def check_open_times():
-        pass
+    def reserve_computer(self, user: User, time_slot: str):
+        """Reserve a computer for the user at a given time slot."""
+        if time_slot in self.reservations:
+            print("Time slot is already reserved.")
+        else:
+            self.reservations[time_slot] = user
+            print(f"Computer reserved for {user.name} at {time_slot}.")
+
+members = ("1234", "2345", "0000")
