8457aa4b04d155f7a87be9f0706a271d546e1171
Aung Aung <aaung8@ivytech.edu>
2024-11-29 00:54:18 -0500

    added create reservation and remove reservation

--- gui.py
+++ gui.py
@@ -1,12 +1,14 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
+from tkcalendar import Calendar
 from user import User
 
 class GUI:
     def __init__(self, root) -> None:
         self.root = root
         root.title("Computer Reservation System")
-        root.geometry("1000x600")
+        root.geometry("600x400")
         root.configure(bg="#ffffff")
         self.large_font = ('Helvetica', 20)
 
@@ -49,14 +51,86 @@ class GUI:
     def create_main_screen(self):
         self.clear_screen()
         
-        tk.Label(self.root, text=f"Logged in as Library Card Number: {self.user.library_card_number}", font=self.large_font).pack()
+        tk.Label(self.root, text=f"Logged in as Library Card Number: {self.user.library_card_number}", font=self.large_font).pack(pady=5)
 
-        self.reserve_computer_button = tk.Button(self.root, text="Reserve Computer", command=self.create_reserve_computer_screen, font=self.large_font).pack()
+        self.reserve_computer_button = tk.Button(self.root, text="Reserve Computer", command=self.create_reserve_computer_screen, font=self.large_font).pack(pady=5)
+        self.cancel_reservation_button = tk.Button(self.root, text="Cancel Computer Reservation", command=self.cancel_computer_reservation_screen, font=self.large_font).pack(pady=5)
 
     def create_reserve_computer_screen(self):
         self.clear_screen()
-        tk.Label(self.root, text="Reserve Computer", font=self.large_font).pack()
+
+        tk.Label(self.root, text="Reserve Computer", font=self.large_font).pack(pady=5)
+        self.cal = Calendar(root, selectmode='day', date_pattern='mm/dd/yy')
+        self.cal.pack(pady=10)
+
+        tk.Label(root, text="Select Time:").pack()
+        time_options = [f"{hour:02d}:{minute:02d}" for hour in range(24) for minute in range(0, 60, 60)]  
+        self.time_combobox = ttk.Combobox(root, values=time_options, state="readonly")
+        self.time_combobox.set("12:00") # default
+        self.time_combobox.pack(pady=5)
+        tk.Button(root, text="Create Reservation", command=self.create_reservation).pack(pady=5)
+
+        tk.Button(root, text="Back", command=self.create_main_screen).pack(pady=5)
+    
+    def create_reservation(self):
+        selected_date = self.cal.get_date()  # Get selected date from calendar
+        selected_time = self.time_combobox.get()  # Get selected time from combobox
+        date_time_string = selected_date + " " + selected_time
+        current_reservations = len(self.user.list_reservations())
+
+        if current_reservations >= 3:
+            messagebox.showerror("Error", f"You have {current_reservations}, you can't have any more")
+            return
         
+        try:
+            self.user.reservation.reserve_computer(date_time_string)
+
+        except TypeError as e:
+            # Library isn't open
+            messagebox.showerror("Error", f"{e}")
+            return
+
+        except ValueError as e:
+            # time slot is in the past
+            messagebox.showerror("Error", f"{e}")
+            return
+        
+        except IndexError as e:
+            # time slot is already reserved
+            messagebox.showerror("Error", f"{e}")
+            return
+        
+        else:
+            messagebox.showinfo("Reservation Success", f"Reservation for {date_time_string} successful!")
+            self.create_main_screen()
+    
+    def cancel_computer_reservation_screen(self):
+        self.clear_screen()
+
+        tk.Label(self.root, text="Cancel Reservation", font=self.large_font).pack(pady=5)
+        reservations = self.user.list_reservations()
+        reservations = [r[0] for r in reservations]
+        tk.Label(root, text="Select Reservation:").pack()
+        self.reservation_combobox = ttk.Combobox(root, values=reservations, state="readonly")
+        self.reservation_combobox.pack(pady=5)
+
+        tk.Button(root, text="Cancel Reservation", command=self.cancel_reservation).pack(pady=5)
+
+        tk.Button(root, text="Back", command=self.create_main_screen).pack(pady=5)
+
+    def cancel_reservation(self):
+        reservation = self.reservation_combobox.get()
+        try:
+            self.user.reservation.cancel_reservation(reservation)  
+        except ValueError as e:
+            # reservation doesn't exist
+            messagebox.showerror("Error", f"{e}")
+            return
+        else:
+            messagebox.showinfo("Cancelation Success", f"Canceled reservation for {reservation}!")
+            self.create_main_screen()
+
+
 
     def clear_screen(self):
         """
@@ -65,8 +139,6 @@ class GUI:
         for widget in self.root.winfo_children():
             widget.destroy()
     
-
-
 
 if __name__ == "__main__":
     root = tk.Tk()
