067eaa62499f20053d0072fdc761d4f2a0819a2a
Aung Aung <aaung8@ivytech.edu>
2024-11-29 12:18:20 -0500

    creates fills the database with mock users

--- initialize_users.py
+++ initialize_users.py
@@ -1,6 +1,11 @@
 from user import User
+import sqlite3
 
 # initializes users in the database for testing. All with the same password
-people = ["0", "1", "2", "3", "4"]
-for person in people:
-    User(person*14, "0000", 1)
+def initialize_users():
+    people = ["0", "1", "2", "3", "4"]
+    for person in people:
+        try:
+            User(person*14, "0000", 1)
+        except sqlite3.IntegrityError:
+            pass
