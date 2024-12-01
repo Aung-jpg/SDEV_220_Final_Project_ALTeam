c842b0257c81b4e721942efa3afdb08f3b1f00b1
Aung Aung <aaung8@ivytech.edu>
2024-12-01 11:34:53 -0500

    made create_db.py to create a database for testing

---
+++ create_db.py
@@ -0,0 +1,8 @@
+from initialize_users import initialize_users
+
+if __name__ == "__main__":
+    """
+    This initializes 5 users all with the pass 0000 for testing.
+    The library card number for each user is just a number (Only numbers 1, 2, 3, 4, and 5) repeated 14 times.
+    """
+    initialize_users()
