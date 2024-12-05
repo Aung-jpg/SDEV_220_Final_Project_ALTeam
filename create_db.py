7bd9a2014d53a0b31217a5871631ddcf998e9afe
Aung Aung <aaung8@ivytech.edu>
2024-12-04 22:44:17 -0500

    Add printed message whenever create_db is ran

--- create_db.py
+++ create_db.py
@@ -1,8 +1,11 @@
 from initialize_users import initialize_users
 
 if __name__ == "__main__":
-    """
-    This initializes 5 users all with the pass 0000 for testing.
-    The library card number for each user is just a number (Only numbers 1, 2, 3, 4, and 5) repeated 14 times.
+    instruction = """
+create_db.py creates a database (meant for testing).
+In the db there are five members that have library card numbers that are a number (0 through 4) repeated 14 times. 
+They all have the same pin of 0000.
+Example: library card number: 33333333333333 and pin: 0000
     """
     initialize_users()
+    print(instruction)
